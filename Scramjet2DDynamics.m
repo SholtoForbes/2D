75e507bcf1891c8a8e406fcdd17f4101e3a63ae7
SholtoForbes <sholtoforbes@hotmail.com>
2015-05-06 13:06:00 +1000

    global Scaling
    shifted to global scaling and started to add real force variables

--- Scramjet2DDynamics.m
+++ Scramjet2DDynamics.m
@@ -17,11 +17,6 @@ omega = primal.states(6,:);
 tau  = primal.controls(1,:); %Thrust
 Mc = primal.controls(2,:); %Control Moment
 
-
-
-% theta  = primal.controls(1,:);
-% tau  = primal.controls(2,:);
-
 %=======================================================
 % Equations of Motion:
 %=======================================================
@@ -31,62 +26,48 @@ m = 1000.;
 Cl = .1;
 Cd = .1;
 rho = 0.02;
-% rho = 1.;
 A = 3.;
 
 %======================================================
 
 
-% Original
-% xdot = vx;
-% ydot = vy;							 
-% vxdot = m*(T.*cos(theta) - 0.5*rho*A*Cl*(vx.^2 + vy.^2).*sin(theta) - 0.5*rho*A*Cd*(vx.^2 + vy.^2).*cos(theta));  
-% vydot = m*(T.*sin(theta) + 0.5*rho*A*Cl*(vx.^2 + vy.^2).*cos(theta) - 0.5*rho*A*Cd*(vx.^2 + vy.^2).*sin(theta) - 9.8);  
+%
 
-
-% scaling test (all other scaling variables currently =1) I need to analyse
-% this better, mine is much more complex than brac problem
-% need to change all scaling variables in this and problem file together
-% X = 10;
-% Y = 10;
-global X;
-global Y;
-VX = 1.;
-VY = 1.;
-TAU = 1.;
-THETA = 1.;
-T = 1.;
-
-hdot = vh*VX*T/X;
-vdot = vv*VY*T/Y;							 
-% vxdot = T/VX/m*(tau*TAU.*cos(theta*THETA) - 0.5*rho*A*Cl*((vx*VX).^2 + (vy*VY).^2).*sin(theta*THETA) - 0.5*rho*A*Cd*((vx*VX).^2 + (vy*VY).^2).*cos(theta*THETA));  
-% vydot = T/VY/m*(tau*TAU.*sin(theta*THETA) + 0.5*rho*A*Cl*((vx*VX).^2 + (vy*VY).^2).*cos(theta*THETA) - 0.5*rho*A*Cd*((vx*VX).^2 + (vy*VY).^2).*sin(theta*THETA) - 9.8);  
-
+hdot = vh;
+vdot = vv;							 
 
 %===========================================================
 % Adding better scramjet dynamics, added 21/4/15
 % communicator matrix is given in terms of forces and moments
 Iy = 1.;
 
-% Fx = -sqrt(vh.^2 + vv.^2); %these will need to come from the communicator matrix, for now just approximated a function that looks close to com mat values
-% Fz = 2*sqrt(vh.^2 + vv.^2);
-% My = 250*sqrt(vh.^2 + vv.^2);
-
-
 % import data from force matrix
 Out_force = dlmread('out_force.txt');
+%an initial interpolator for the force values at a fixed Arot, alpha and
+%dynamic pressure (0,  -0.0174532925199 (negative up) , 45000.0)
+
+M_array = [4.5 , 5. , 5.5]; 
+Fx_array = [-36427.6593981 , -42995.3909773 , -50209.1507264];
+Fz_array = [ 26851.676829 , 25865.7310572 , 24420.6025981 ];
+My_array = [305002.235162 , 256125.242712 , 196654.950117 ];
+
+
+Fx = interp1(M_array, Fx_array, vv/1000) % These give NaN values if not within range
+Fz = interp1(M_array, Fx_array, vv/1000)
 
 
 
-Fx = -sqrt(vh.^2 + vv.^2); 
-% Fx = 0.; 
-% Fz = 0.;
-Fz = -0.1*sqrt(vh.^2 + vv.^2); 
+% % Fx = -sqrt(vh.^2 + vv.^2); 
+% Fx = -50000.;
+% Fz = -0.1*sqrt(vh.^2 + vv.^2); 
+% % Fz = 0.;
 My = 0.;
 
 
-vhdot = (Fx.*cos(theta) + Fz.*sin(theta)  + tau.*cos(theta))/m;
-vvdot = (-Fx.*sin(theta) + Fz.*cos(theta)  + tau.*sin(theta))/m;
+
+
+vhdot = (Fx.*cos(theta) - Fz.*sin(theta)  + tau.*cos(theta))/m;
+vvdot = (Fx.*sin(theta) + Fz.*cos(theta)  + tau.*sin(theta))/m;
 thetadot = omega;
 omegadot = (My  + Mc)/Iy;
 %====================================================================
