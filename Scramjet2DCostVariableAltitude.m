e2fab0f52970a6f42fe222ea6c01ab4d05e0bdd9
SholtoForbes <sholtoforbes@hotmail.com>
2015-06-03 11:08:42 +1000

    removed v again

--- Scramjet2DCostVariableAltitude.m
+++ Scramjet2DCostVariableAltitude.m
@@ -6,112 +6,113 @@ function [EndpointCost, RunningCost] = Brac1Cost(primal)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 global Scale
 
-% =======================================================
-% Vehicle Model:
-% =======================================================
-VScaled = primal.states(1,:) ; 
-HScaled = primal.states(2,:) ; 
-vScaled = primal.states(3,:) ; 
-
-V = VScaled * Scale;
-H = HScaled * Scale;
-v = vScaled * Scale;
-
-theta  = primal.controls(1,:); %acceleration in x plane
-
-timeScaled = primal.nodes(1,:);
-time = timeScaled*Scale;
-
-dt_array = time(2:end)-time(1:end-1); % Length of each timestep
-
-%%%% NEED TO COMPUTE VELOCITY, CREATE THRUST TERM
-
-
-%neglecting AoA, fixed reference frame
-m = 5000.;
-Cl = .1;
-Cd = .1;
-rho = 0.02;
-A = 3.;
-g = 9.81;
-
-%======================================================
-% Adding better scramjet dynamics, added 21/4/15
-% communicator matrix is given in terms of forces and moments
-
-Iy = 1.; %%%% CHANGE THIS
-
-% import data from force matrix
-Out_force = dlmread('out_force.txt');
-
-% import data from atmosphere matrix
-Atmosphere = dlmread('atmosphere.txt');
-
-% Calculate ablsolute height
-StartingV = 25000; % THIS NEEDS TO BE CHANGED FOR VARIABLE HEIGHT
-Vabs = V + StartingV;
-
-% Calculate speed of sound using atmospheric data
-c = spline( Atmosphere(:,1),  Atmosphere(:,5), Vabs);
-
-% Calculate density using atmospheric data
-rho = spline( Atmosphere(:,1),  Atmosphere(:,4), Vabs);
-
-%an initial interpolator for the force values at a fixed Arot, alpha and
-%dynamic pressure (0,  -0.0174532925199 (negative up) , 45000.0)
-
-M_array = [4.5 , 5. , 5.5]; 
-Fd_array = [-36427.6593981 , -42995.3909773 , -50209.1507264];
-Flift_array = [ 26851.676829 , 25865.7310572 , 24420.6025981 ];
-My_array = [305002.235162 , 256125.242712 , 196654.950117 ];
-
-
-% For each alpha, spline force results for current dynamic pressure and
-% Mach no
-
-% AoA1 = [Out_force(1,:);Out_force(4,:);Out_force(7,:)];
-% AoA1_spline = [spline(AoA1(:,3), AoA1(:,6), M)];
-
-%WORK IN PROGRESS
-
-% NEED TO INTRODUCE AoA FROM LIFT FORCE EQUALITY AND RESOLVE THRUST AND
-% MOMENT
-
-
-% THRUST AND MOTION ==================================================================
-global M
-
-% Calculating Mach No (Descaled)
-
-M = v./c ;
-
-Fd = spline(M_array, Fd_array, M);
-Flift = spline(M_array, Flift_array, M)  ;
-My = spline(M_array, My_array, M)  ;
-
-% Calculating Dynamic Pressure
-q = 0.5 * rho .* (v .^2);
-
-Thrust =  - Fd + g*sin(theta) + 10000; % PLACEHOLDER TERM FOR CONSTANT ACCELERATION
-
-a = Thrust - (- Fd + g*sin(theta)) / m ; % acceleration
-
-%===========================================================================
-
-
-% Efficiency CHANGE THIS TO DYNAMIC PRESSURE RATHER THAN M
-% Efficiency = (-(M(1:end-1)-5.).^2 +25.)/25.; % this is a simple inverse parabola centred around M=5 and going to zero at M=0 and M=10 and scaled so that it varies between 1 and 0
-% Efficiency = (-(M(1:end-1)-8).^2 + 80.)/80.; % increasing the added value gives a smoother function
-Efficiency = 1;
-
-%Fuel rate of change
-Fueldt = Thrust(1:end-1) ./ Efficiency; % Temporary fuel rate of change solution, directly equated to thrust (should give correct efficiency result, but cannot analyse total fuel change accurately)
-
-
-fuelchange_array = -Fueldt.*dt_array ; %Fuel change over each timestep
-
-dfuel = sum(fuelchange_array); %total change in 'fuel' this is negative
-
+% % =======================================================
+% % Vehicle Model:
+% % =======================================================
+% VScaled = primal.states(1,:) ; 
+% HScaled = primal.states(2,:) ; 
+% vScaled = primal.states(3,:) ; 
+% 
+% V = VScaled * Scale;
+% H = HScaled * Scale;
+% v = vScaled * Scale;
+% 
+% theta  = primal.controls(1,:); %acceleration in x plane
+% 
+% timeScaled = primal.nodes(1,:);
+% time = timeScaled*Scale;
+% 
+% dt_array = time(2:end)-time(1:end-1); % Length of each timestep
+% 
+% %%%% NEED TO COMPUTE VELOCITY, CREATE THRUST TERM
+% 
+% 
+% %neglecting AoA, fixed reference frame
+% m = 5000.;
+% Cl = .1;
+% Cd = .1;
+% rho = 0.02;
+% A = 3.;
+% g = 9.81;
+% 
+% %======================================================
+% % Adding better scramjet dynamics, added 21/4/15
+% % communicator matrix is given in terms of forces and moments
+% 
+% Iy = 1.; %%%% CHANGE THIS
+% 
+% % import data from force matrix
+% Out_force = dlmread('out_force.txt');
+% 
+% % import data from atmosphere matrix
+% Atmosphere = dlmread('atmosphere.txt');
+% 
+% % Calculate ablsolute height
+% StartingV = 25000; % THIS NEEDS TO BE CHANGED FOR VARIABLE HEIGHT
+% Vabs = V + StartingV;
+% 
+% % Calculate speed of sound using atmospheric data
+% c = spline( Atmosphere(:,1),  Atmosphere(:,5), Vabs);
+% 
+% % Calculate density using atmospheric data
+% rho = spline( Atmosphere(:,1),  Atmosphere(:,4), Vabs);
+% 
+% %an initial interpolator for the force values at a fixed Arot, alpha and
+% %dynamic pressure (0,  -0.0174532925199 (negative up) , 45000.0)
+% 
+% M_array = [4.5 , 5. , 5.5]; 
+% Fd_array = [-36427.6593981 , -42995.3909773 , -50209.1507264];
+% Flift_array = [ 26851.676829 , 25865.7310572 , 24420.6025981 ];
+% My_array = [305002.235162 , 256125.242712 , 196654.950117 ];
+% 
+% 
+% % For each alpha, spline force results for current dynamic pressure and
+% % Mach no
+% 
+% % AoA1 = [Out_force(1,:);Out_force(4,:);Out_force(7,:)];
+% % AoA1_spline = [spline(AoA1(:,3), AoA1(:,6), M)];
+% 
+% %WORK IN PROGRESS
+% 
+% % NEED TO INTRODUCE AoA FROM LIFT FORCE EQUALITY AND RESOLVE THRUST AND
+% % MOMENT
+% 
+% 
+% % THRUST AND MOTION ==================================================================
+% global M
+% 
+% % Calculating Mach No (Descaled)
+% 
+% M = v./c ;
+% 
+% Fd = spline(M_array, Fd_array, M);
+% Flift = spline(M_array, Flift_array, M)  ;
+% My = spline(M_array, My_array, M)  ;
+% 
+% % Calculating Dynamic Pressure
+% q = 0.5 * rho .* (v .^2);
+% 
+% Thrust =  - Fd + g*sin(theta) + 10000; % PLACEHOLDER TERM FOR CONSTANT ACCELERATION
+% 
+% 
+% a = Thrust - (- Fd + g*sin(theta)) / m ; % acceleration
+% 
+% %===========================================================================
+% 
+% 
+% % Efficiency CHANGE THIS TO DYNAMIC PRESSURE RATHER THAN M
+% % Efficiency = (-(M(1:end-1)-5.).^2 +25.)/25.; % this is a simple inverse parabola centred around M=5 and going to zero at M=0 and M=10 and scaled so that it varies between 1 and 0
+% % Efficiency = (-(M(1:end-1)-8).^2 + 80.)/80.; % increasing the added value gives a smoother function
+% Efficiency = 1;
+% 
+% %Fuel rate of change
+% Fueldt = Thrust(1:end-1) ./ Efficiency; % Temporary fuel rate of change solution, directly equated to thrust (should give correct efficiency result, but cannot analyse total fuel change accurately)
+% 
+% 
+% fuelchange_array = -Fueldt.*dt_array ; %Fuel change over each timestep
+% 
+% dfuel = sum(fuelchange_array); %total change in 'fuel' this is negative
+% 
 
 % Define Cost =======================================================
 
